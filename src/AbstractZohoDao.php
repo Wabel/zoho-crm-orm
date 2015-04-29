bebe1cf23bbeac5d0185f5d109ec0acb09d063f8
Michaël Wizman <michaelwizman@hotmail.com>
2015-04-29 12:09:02 +0200

    Correcting the Date format when converting to XML

--- src/AbstractZohoDao.php
+++ src/AbstractZohoDao.php
@@ -152,7 +152,7 @@ abstract class AbstractZohoDao
                     switch ($params['type']) {
                         case "Date":
                             /** @var \DateTime $value */
-                            $value = $value->format('M/d/Y');
+                            $value = $value->format('m/d/Y');
                             break;
                         case "DateTime":
                             /** @var \DateTime $value */
